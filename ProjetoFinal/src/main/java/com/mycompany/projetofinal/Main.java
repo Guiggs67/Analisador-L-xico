eb19a13cff482ab0966c0322bbc1c50fc7d06d41
Wendell9 <guindaniwendell90@gmail.com>
2025-08-29 18:26:30 -0300

    adaptando analisador para ValidadorRGCPF

--- ProjetoFinal/src/main/java/com/mycompany/projetofinal/Main.java
+++ ProjetoFinal/src/main/java/com/mycompany/projetofinal/Main.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Lê o arquivo Java como entrada do analisador
-      FileReader reader = new FileReader("src/main/java/com/mycompany/projetofinal/ProjetoFinal.java");
+      FileReader reader = new FileReader("src/main/java/com/mycompany/projetofinal/ValidadorRGCPF.java");
       Lexer lexer = new Lexer(reader);
 
 
